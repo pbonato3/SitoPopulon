cf952991a73895fd3e51d8d686204245bc05475e
TommasoPadovan <tommaso.pado@gmail.com>
2016-05-02 13:21:08 +0200

    Fixato encoding pagina aggiungi notizie
    inserita funzione moltomaggica piena di $_ e @_

--- cgi-bin/funzioni.pm
+++ cgi-bin/funzioni.pm
@@ -1,5 +1,5 @@
 use XML::LibXML;
-
+use Encode qw(decode encode);
 
 ######### Inserimento nuova notizia ########
 sub nuovaNotizia{
@@ -7,6 +7,13 @@ sub nuovaNotizia{
 	my $parser = XML::LibXML->new();		#creazione oggetto parser
 	my $doc = $parser->parse_file($file);	#apertura file e lettura input
 	my $root= $doc->getDocumentElement;		#estrazione elemento radice
+
+
+
+	foreach (@_) {										#magia, non toccare, che va.
+		$_=encode('UTF-8', $_, Encode::FB_CROAK);		#serve a codificare tutte le stringhe in input in UTF8 (accenti)
+	}
+	
 
 	#creo una stringa con un nuovo elemento
 	if(!$_[0]){$esito="Operazione fallita: Il titolo &egrave obbligatorio"; return $esito;}
@@ -19,13 +26,13 @@ sub nuovaNotizia{
 		if($_[4]){$nuovo.="\n<descrizione>".$_[4]."</descrizione>";};
 		$nuovo.="\n</notizia>";
 
-	$frammento = $parser->parse_balanced_chunk($nuovo);	#controllo la buona formazione e creo un nodo
-	$root->insertBefore($frammento,$root->firstChild);	#inserisco un nuovo in testa alla lista (prima del primo figlio)
-	open OUT, ">$file";			#apro l'output sul file xml
-	print OUT $doc->toString;	#stampo sul file il documento parsato
-	
-	$esito="Operazione completata";
-	return $esito;
+		$frammento = $parser->parse_balanced_chunk($nuovo);	#controllo la buona formazione e creo un nodo
+		$root->insertBefore($frammento,$root->firstChild);	#inserisco un nuovo in testa alla lista (prima del primo figlio)
+		open OUT, ">$file";			#apro l'output sul file xml
+		print OUT $doc->toString;	#stampo sul file il documento parsato
+		
+		$esito="Operazione completata";
+		return $esito;
 	};
 	$esito = "operazione fallita";
 	return $esito;
