7f6a268950d256a12f56fd9fbef7af2f8c3e66a2
biggeb4 <biggeb4@gmail.com>
2016-05-25 12:11:25 +0200

    prima funzione javascript

--- PopScripts.js
+++ PopScripts.js
@@ -1,1 +1,10 @@
-
+ 
+var selectedRazza = document.getElementById('SelRazza'); 
+ 
+selectedRazza.addEventListener('change', function() { 
+  document.getElementById('immagineRazza').src = selezionaImg(this.value); 
+}); 
+ 
+function selezionaimg(razza){ 
+  return "img/"+razza+".jpg" 
+} 
