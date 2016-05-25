3b422ae8dc8d1767e3450a6e40fe3d80b048261d
biggeb4 <biggeb4@gmail.com>
2016-05-25 19:13:15 +0200

    aggiornamento funzionamento del bonus

--- PopScripts.js
+++ PopScripts.js
@@ -16,20 +16,23 @@ function selezionaimg(razza){
 
 var settedLevelAC = document.getElementById('setLevelAC'); 
 settedLevel.addEventListener('change', function() { 
-	if(document.getElementById('corpo')!= null){
+	if(document.getElementById('corpo')!= null && document.getElementById('setLevelAC') < 5){
   		document.getElementById('setLevelAC').src = setLevelAC+setBonus("corpo")
 	} 
 }); 
 
 var settedLevelAC = document.getElementById('corpo'); 
 settedLevel.addEventListener('change', function() { 
-	if(document.getElementById('setLevelAC')!= null){
+	if(document.getElementById('setLevelAC')!= null && document.getElementById('setLevelAC') < 5){
   		document.getElementById('setLevelAC').src = setLevelAC+setBonus("corpo")
 	} 
 }); 
  
 function setBonus(tipo){ 
   	if(tipo=="corpo"){
-		return Math.floor(document.getElementById('corpo')-20) / 10);
+		if(document.getElementById('corpo')-20 > 0)
+		return 1
+		else
+		return 0
 	} 
 } 
