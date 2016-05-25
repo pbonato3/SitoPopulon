cc68516242b143db703b1883d6990104fcda1214
biggeb4 <biggeb4@gmail.com>
2016-05-25 17:44:53 +0200

    aggiunta funzione punti abiltà corpo

--- PopScripts.js
+++ PopScripts.js
@@ -1,4 +1,9 @@
  
+document.addEventListener('DOMContentLoaded', function(event) {
+    cookieChoices.showCookieConsentBar('Il tuo messaggio per i visitarori.',
+        'Chiudi', 'Maggiori Informazioni');
+  });
+
 var selectedRazza = document.getElementById('SelRazza'); 
  
 selectedRazza.addEventListener('change', function() { 
@@ -7,4 +12,24 @@ selectedRazza.addEventListener('change', function() {
  
 function selezionaimg(razza){ 
   return "img/"+razza+".jpg" 
+} 
+
+var settedLevelAC = document.getElementById('setLevelAC'); 
+settedLevel.addEventListener('change', function() { 
+	if(document.getElementById('corpo')!= null){
+  		document.getElementById('setLevelAC').src = setLevelAC+setBonus("corpo")
+	} 
+}); 
+
+var settedLevelAC = document.getElementById('corpo'); 
+settedLevel.addEventListener('change', function() { 
+	if(document.getElementById('setLevelAC')!= null){
+  		document.getElementById('setLevelAC').src = setLevelAC+setBonus("corpo")
+	} 
+}); 
+ 
+function setBonus(tipo){ 
+  	if(tipo=="corpo"){
+		return Math.floor(document.getElementById('corpo')-20) / 10);
+	} 
 } 
